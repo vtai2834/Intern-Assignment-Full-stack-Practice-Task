2c3210931c2e77383ba135c2c8b7bd367ab370aa
Phan Van Tai <vantai2004kt@gmail.com>
2025-11-06 12:48:54 +0700

    fix: configurate to connect redis cloud + local

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -7,8 +7,10 @@ export const connectRedis = async () => {
     redisClient = createClient({
       socket: {
         host: process.env.REDIS_HOST || 'localhost',
-        port: process.env.REDIS_PORT || 6379
+        port: process.env.REDIS_PORT || 6379,
+        tls: true
       },
+      username: process.env.REDIS_USERNAME || undefined,
       password: process.env.REDIS_PASSWORD || undefined
     });
 
