13b0b232c579ed7cbb7fdf86b110111211c61be0
Phan Van Tai <vantai2004kt@gmail.com>
2025-11-06 13:02:15 +0700

    fix: fix redis client can connect both cloud + local url

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -4,14 +4,18 @@ let redisClient = null;
 
 export const connectRedis = async () => {
   try {
+    const isCloud = !!process.env.REDIS_HOST && process.env.REDIS_HOST.includes('redns.redis-cloud.com');
+
+    // Nếu là Redis Cloud → dùng rediss:// (TLS)
+    const url = isCloud
+      ? `rediss://${process.env.REDIS_USERNAME || 'default'}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
+      : `redis://${process.env.REDIS_HOST || 'localhost'}:${process.env.REDIS_PORT || 6379}`;
+
     redisClient = createClient({
-      socket: {
-        host: process.env.REDIS_HOST || 'localhost',
-        port: process.env.REDIS_PORT || 6379,
-        tls: true
-      },
-      username: process.env.REDIS_USERNAME || undefined,
-      password: process.env.REDIS_PASSWORD || undefined
+      url,
+      socket: isCloud
+        ? { rejectUnauthorized: false } // Bỏ verify cert nếu là Redis Cloud
+        : {}, // Local thì không cần TLS
     });
 
     redisClient.on('error', (err) => {
